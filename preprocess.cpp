11bfe64daf6d57cc82b8b3d1eeffcdcc6f91023d
Marion Sudvarg <msudvarg@node25.engr.wustl.edu>
2024-01-03 19:27:12 -0600

    Correct output for 5 ALPHAs. Hopefully it synthesizes

--- preprocess.cpp
+++ preprocess.cpp
@@ -159,11 +159,11 @@ extern "C" {
             struct Centroid *centroid // Output Centroid
 	        )
     {
-#pragma HLS INTERFACE m_axi depth=1 port=input_data_packet bundle=aximm1
-#pragma HLS INTERFACE m_axi depth=8192 port=input_all_peds bundle=aximm2
+#pragma HLS INTERFACE m_axi depth=5 port=input_data_packet bundle=aximm1
+#pragma HLS INTERFACE m_axi depth=40960 port=input_all_peds bundle=aximm2
 #pragma HLS INTERFACE m_axi depth=8 port=bounds bundle=aximm3
 #pragma HLS INTERFACE m_axi depth=4 port=zero_thresholds bundle=aximm4
-#pragma HLS INTERFACE m_axi depth=64 port=output_integrals bundle=aximm5
+#pragma HLS INTERFACE m_axi depth=320 port=output_integrals bundle=aximm5
 #pragma HLS INTERFACE m_axi depth=1 port=centroid bundle=aximm6
 
         for (unsigned alpha = 0; alpha < NUM_ALPHAS; ++alpha) {
--- preprocess.h
+++ preprocess.h
@@ -16,7 +16,7 @@ extern "C" {
 #define NUM_SAMPLES 256 // N
 #define BUF_SIZE 4105 // 8 + N*16 + 1 words (16 bits / 2 bytes per word)
 #define NUM_INTEGRALS 4
-#define NUM_ALPHAS 1
+#define NUM_ALPHAS 5
 
 /*
  Mimics incoming data packet in C types.
