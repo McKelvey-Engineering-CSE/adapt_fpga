f188943b2d09f86674cd8d376998c8f35956d515
Marion Sudvarg <msudvarg@node25.engr.wustl.edu>
2024-01-05 15:57:03 -0600

    Separating zeroed_integrals into a different array for better pipelining. Achieves total interval 22187

--- preprocess.cpp
+++ preprocess.cpp
@@ -11,10 +11,13 @@
 
 int16_t ped_sub_results[NUM_ALPHAS][NUM_SAMPLES][NUM_CHANNELS]; // Really 13 bits
 int32_t integrals[NUM_ALPHAS][NUM_INTEGRALS][NUM_CHANNELS];
+int32_t zeroed_integrals[NUM_ALPHAS][NUM_INTEGRALS][NUM_CHANNELS];
 Centroid local_centroid;
 
 
 void ped_subtract(const SW_Data_Packet * pkt, const uint16_t *peds, const uint8_t a) {
+
+    #pragma HLS ARRAY_PARTITION variable=ped_sub_results type=complete dim=3
 
     const uint8_t starting_sample_number = pkt->starting_sample_number;
     const uint8_t bank = pkt->bank;
@@ -31,6 +34,8 @@ void ped_subtract(const SW_Data_Packet * pkt, const uint16_t *peds, const uint8_
 }
 
 void integrate(const SW_Data_Packet * pkt, const int16_t *bounds, const uint8_t a) {
+
+    #pragma HLS ARRAY_PARTITION variable=ped_sub_results type=complete dim=3
 
     //Assume fine_time > starting_sample_number, so base_addr is positive
     int16_t base_addr = pkt->fine_time - pkt->starting_sample_number;
@@ -111,7 +116,7 @@ void zero_suppress(const int32_t * thresholds, const uint8_t a) {
             int32_t integral = integrals[a][i][c];
             const int32_t threshold = thresholds[i];
             integral = (integral < threshold) ? 0 : integral;
-            integrals[a][i][c] = integral;
+            zeroed_integrals[a][i][c] = integral;
         }
     }
 }
@@ -123,7 +128,7 @@ int16_t island_detection(const uint8_t integral_num) {
         #pragma HLS UNROLL factor=5
         island_channels: for (uint8_t c = 0; c < NUM_CHANNELS; ++c) {
             #pragma HLS UNROLL factor=16
-            int32_t integral_val = integrals[a][integral_num][c];
+            int32_t integral_val = zeroed_integrals[a][integral_num][c];
             if(integral_val && !in_island) {
                 in_island = true;
                 ++num_islands;
@@ -148,7 +153,7 @@ int16_t centroiding(Centroid * centroid, const uint8_t integral_num) {
         centroiding_channels: for (unsigned c = 0; c < NUM_CHANNELS; ++c) {
             #pragma HLS UNROLL factor=16
             const uint16_t pos = a * NUM_CHANNELS + c;
-            const int32_t integral = integrals[a][integral_num][c];
+            const int32_t integral = zeroed_integrals[a][integral_num][c];
             position += pos * integral;
             signal += integral;
         }
@@ -160,7 +165,7 @@ int16_t centroiding(Centroid * centroid, const uint8_t integral_num) {
 
 void write_outputs(Centroid * centroid, int32_t * output_integrals) {
     *centroid = local_centroid;
-    memcpy(output_integrals, integrals, NUM_ALPHAS*NUM_INTEGRALS*NUM_CHANNELS*sizeof(int32_t));
+    memcpy(output_integrals, zeroed_integrals, NUM_ALPHAS*NUM_INTEGRALS*NUM_CHANNELS*sizeof(int32_t));
 }
 
 extern "C" {
@@ -174,7 +179,7 @@ extern "C" {
 	        )
     {
 #pragma HLS INTERFACE m_axi depth=1 port=input_data_packet bundle=aximm1
-#pragma HLS array_partition variable=input_data_packet type=block factor=5
+// #pragma HLS array_partition variable=input_data_packet type=block factor=5
 #pragma HLS INTERFACE mode=bram depth=40960 port=input_all_peds
 #pragma HLS INTERFACE mode=bram depth=1 port=bounds
 #pragma HLS array_partition variable=bounds type=block factor=4
