53e565446586f42e66cc2eb09c7db7093a27672f
Marion Sudvarg <msudvarg@ssh8-2.engr.wustl.edu>
2024-03-08 17:46:46 -0600

    filepath.h modified for generic use

--- filepath.h
+++ filepath.h
@@ -4,8 +4,7 @@
 #include <string>
 
 //TODO: Update the following path!
-// const std::string path_to_repo = "/home/yourusername/adapt_fpga/";
-const std::string path_to_repo = "/home/warehouse/msudvarg/capstone_sp23/src/";
+const std::string path_to_repo = "/home/username/adapt_fpga/";
 const std::string str_packet_file = path_to_repo + "EventStream.dat";
 const std::string str_ped_file = path_to_repo + "peds.dat";
 const std::string str_output_file = path_to_repo + "output.txt";
