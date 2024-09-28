ad0e137a808a292d72f4274a2bf45b5cdcb11e26
Marion Sudvarg <msudvarg@gmail.com>
2024-09-28 10:59:24 -0500

    Easier way to define filepaths pulled in from artifact git repo

---
+++ filepath.h
@@ -0,0 +1,14 @@
+#ifndef FILEPATH_H
+#define FILEPATH_H
+
+#include <string>
+
+//TODO: Update the following path!
+const std::string path_to_repo = "C:/Users/msudv/Documents/Development/ADAPT/FPGA_Motherboard/adapt_fpga/";
+const std::string str_packet_file = path_to_repo + "EventStream.dat";
+const std::string str_ped_file = path_to_repo + "peds.dat";
+const std::string str_output_file = path_to_repo + "output.txt";
+const char * packet_file = str_packet_file.c_str();
+const char * ped_file = str_ped_file.c_str();
+const char * output_file = str_output_file.c_str();
+#endif
--- host_hls.cpp
+++ host_hls.cpp
@@ -162,14 +162,14 @@ int peds_dat_to_arrays(int fd, vec_uint16_16 * all_peds){
 }
 
 int initialize_inputs(struct SW_Data_Packet * data_packet, vec_uint16_16 * all_peds) {
-    int data_packet_fd = open("/home/warehouse/msudvarg/capstone_sp23/src/EventStream.dat", 0, "r");
+    int data_packet_fd = open(str_packet_file, 0, "r");
     if (data_packet_fd == -1) {
         perror("open");
     }
 
     data_packet_dat_to_struct(data_packet_fd, data_packet);
 
-    int peds_fd = open("/home/warehouse/msudvarg/capstone_sp23/src/peds.dat", 0, "r");
+    int peds_fd = open(str_ped_file, 0, "r");
     if (peds_fd == -1) {
         perror("open");
     }
@@ -225,7 +225,7 @@ int write_output(int fd, const char ** bounds, vec_int32_16 *integrals, struct S
 }
 
 int produce_output(const char ** bounds, vec_int32_16 *integrals, struct SW_Data_Packet * data_packet) {
-    int output_fd = open("/home/research/msudvarg/capstone_sp23/src/output.txt", O_CREAT | O_RDWR, 0666);
+    int output_fd = open(str_output_file, O_CREAT | O_RDWR, 0666);
     if (output_fd == -1) {
         perror("open");
     }
@@ -300,7 +300,7 @@ int main()
                 (struct Centroid *) &centroid
                 );
 
-    int output_fd = open("/home/research/msudvarg/capstone_sp23/src/output.txt", O_CREAT | O_RDWR, 0666);
+    int output_fd = open(str_output_file, O_CREAT | O_RDWR, 0666);
     if (output_fd == -1) {
         perror("open");
     }
